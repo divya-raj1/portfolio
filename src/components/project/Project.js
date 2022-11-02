71f56652037a3de94e105b5ebb8e893db3a687ee
divya.raj <divyarj98@gmail.com>
2022-11-02 13:53:33 +0530

    Added one more project

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Project.css';
 import Project1 from '../../assets/Home/portfolio.png';
-import Project3 from '../../assets/Home/react-api-image.png';
-import Project4 from '../../assets/Home/priceTracker.png';
+import Project4 from '../../assets/Home/react-api-image.png';
+import Project5 from '../../assets/Home/priceTracker.png';
 import Project2 from '../../assets/Home/playstore.png';
+import Project3 from '../../assets/Home/ReactCRUD.png';
 
 
 //array of images
@@ -25,13 +26,20 @@ const data = [
   {
     id: 3,
     image: Project3,
+    title: 'React CRUD',
+    github: 'https://github.com/divya-raj1/react-crud',
+    demo: 'https://divya-raj1.github.io/react-crud/'
+  },
+  {
+    id: 4,
+    image: Project4,
     title: 'Responsive React Webpage',
     github: 'https://github.com/divya-raj1/react-webpage',
     demo: 'https://divya-raj1.github.io/react-webpage/'
   },
   {
-    id: 4,
-    image: Project4,
+    id: 5,
+    image: Project5,
     title: 'Simple price tracker',
     github: 'https://github.com/divya-raj1/Price-Tracker',
     demo: 'https://divya-raj1.github.io/Price-Tracker/'
@@ -54,7 +62,7 @@ export default function Portfolio() {
               <h3>{title}</h3>
               <div className="project-item-cta">
                 <a href={github} className="btn" target="_blank">Github</a>
-                <a href={demo} className="btn btn-primary" target={id == 1 ? "" : "_blank"}>Demo</a>
+                <a href={demo} className="btn btn-primary" target={id === 1 ? "" : "_blank"}>Demo</a>
               </div>
             </article>
             )
